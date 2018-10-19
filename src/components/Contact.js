c388ec9f9f96cbbecc0f159dd758ae5512de01c4
Olufemi Afolabi <olu@Olufemis-MacBook-Pro.local>
2018-10-19 00:32:01 -0500

    address included in contact details

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ class Contact extends Component {
     showContactInfo: false
   };
   render() {
-    const { name, email, phone } = this.props.contact;
+    const { name, email, phone, address } = this.props.contact;
     const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
@@ -24,6 +24,7 @@ class Contact extends Component {
           <ul className="list-group">
             <li className="list-group-item">Email: {email}</li>
             <li className="list-group-item">Phone: {phone}</li>
+            <li className="list-group-item">Address: {address}</li>
           </ul>
         ) : null}
       </div>
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -7,19 +7,22 @@ class Contacts extends Component {
         id: 1,
         name: "John Doe",
         email: "jdoe@gmail.com",
-        phone: "555-555-5555"
+        phone: "555-555-5555",
+        address: "12 artesian ave"
       },
       {
         id: 2,
         name: "Karen Williams",
         email: "karenw@gmail.com",
-        phone: "444-444-4445"
+        phone: "444-444-4445",
+        address: "22 western ave"
       },
       {
         id: 3,
         name: "Mark Daniel",
         email: "markd@gmail.com",
-        phone: "465-465-4655"
+        phone: "465-465-4655",
+        address: "34 kigil ave"
       }
     ]
   };
